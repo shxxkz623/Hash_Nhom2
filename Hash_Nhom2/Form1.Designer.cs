9bb2441e33c192a3f58fb8fb25ed01bdbf6d3bc8
shxxkt623 <kimthoalt2306@gmail.com>
2022-11-09 10:02:09 +0700

    Add project files.

---
+++ Hash_Nhom2/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿
+namespace Hash_Nhom2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.tbMD5 = new System.Windows.Forms.TextBox();
+            this.tbSHA1 = new System.Windows.Forms.TextBox();
+            this.tbSHA256 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // textBox
+            // 
+            this.textBox.Location = new System.Drawing.Point(139, 22);
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(509, 27);
+            this.textBox.TabIndex = 1;
+            this.textBox.Text = "UIT Cryptography";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(654, 22);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(38, 29);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(24, 65);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(63, 24);
+            this.checkBox1.TabIndex = 3;
+            this.checkBox1.Text = "MD5";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(24, 104);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(74, 24);
+            this.checkBox2.TabIndex = 4;
+            this.checkBox2.Text = "SHA-1";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(24, 147);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(90, 24);
+            this.checkBox3.TabIndex = 5;
+            this.checkBox3.Text = "SHA-256";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // tbMD5
+            // 
+            this.tbMD5.Location = new System.Drawing.Point(139, 65);
+            this.tbMD5.Name = "tbMD5";
+            this.tbMD5.Size = new System.Drawing.Size(553, 27);
+            this.tbMD5.TabIndex = 6;
+            // 
+            // tbSHA1
+            // 
+            this.tbSHA1.Location = new System.Drawing.Point(139, 104);
+            this.tbSHA1.Name = "tbSHA1";
+            this.tbSHA1.Size = new System.Drawing.Size(553, 27);
+            this.tbSHA1.TabIndex = 7;
+            // 
+            // tbSHA256
+            // 
+            this.tbSHA256.Location = new System.Drawing.Point(139, 144);
+            this.tbSHA256.Name = "tbSHA256";
+            this.tbSHA256.Size = new System.Drawing.Size(553, 27);
+            this.tbSHA256.TabIndex = 8;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(299, 195);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(94, 29);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Calculate";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(24, 22);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(102, 28);
+            this.comboBox1.TabIndex = 10;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(710, 262);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.tbSHA256);
+            this.Controls.Add(this.tbSHA1);
+            this.Controls.Add(this.tbMD5);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.TextBox tbMD5;
+        private System.Windows.Forms.TextBox tbSHA1;
+        private System.Windows.Forms.TextBox tbSHA256;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ Hash_Nhom2/Form1.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace Hash_Nhom2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+        public static String inputHash;
+        public static byte[] arrayBytes;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            DialogResult dr = ofd.ShowDialog();
+            if (dr == DialogResult.OK)
+            {
+                FileStream fs = new FileStream(ofd.FileName, FileMode.OpenOrCreate);
+                StreamReader sr = new StreamReader(fs);
+                string content = sr.ReadToEnd();
+                textBox.Text = content;
+                fs.Close();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox1.Items.Add("Binary file");
+            comboBox1.Items.Add("Text string");
+            comboBox1.Items.Add("Hex string");
+            comboBox1.SelectedIndex = 1;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedIndex != 0)
+            {
+                button1.Enabled = false;
+                textBox.Enabled = true;
+            }
+            else
+            {
+                button1.Enabled = true;
+                textBox.Text = "";
+                textBox.Enabled = false;
+            }
+        }
+        public static string CreateMD5(byte[] inputBytes)
+        {
+            // Use input string to calculate MD5 hash
+            MD5 md5 = MD5.Create();
+            {
+                byte[] hashBytes = md5.ComputeHash(inputBytes);
+
+                // Convert the byte array to hexadecimal string
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < hashBytes.Length; i++)
+                {
+                    sb.Append(hashBytes[i].ToString("X2"));
+                }
+                return sb.ToString();
+            }
+        }
+
+        public static byte[] FromHexString(string hexString)
+        {
+            var bytes = new byte[hexString.Length / 2];
+            for (var i = 0; i < bytes.Length; i++)
+            {
+                bytes[i] = Convert.ToByte(hexString.Substring(i * 2, 2), 16);
+            }
+
+            return bytes;
+        }
+
+        /*public static string GetBytesFromBinaryString(String input)
+        {
+            string result = "";
+            while (input.Length > 0)
+            {
+                var first8 = input.Substring(0, 8);
+                input = input.Substring(8);
+                var number = Convert.ToInt32(first8, 2);
+                result += (char)number;
+            }
+            return result;
+        }*/
+
+        public byte[] GetBytesFromBinaryString(String binary)
+        {
+            var list = new List<Byte>();
+
+            for (int i = 0; i < binary.Length; i += 8)
+            {
+                String t = binary.Substring(i, 8);
+
+                list.Add(Convert.ToByte(t, 2));
+            }
+
+            return list.ToArray();
+        }
+
+        public static string CreateSHA1(byte[] inputBytes)
+        {
+            // Use input string to calculate MD5 hash
+            SHA1 sha1 = SHA1.Create();
+            {
+                byte[] hashBytes = sha1.ComputeHash(inputBytes);
+                string hash = BitConverter.ToString(hashBytes).Replace("-", String.Empty);
+                return hash;
+            }
+        }
+
+        public static string CreateSHA2(byte[] inputBytes)
+        {
+            // Use input string to calculate MD5 hash
+            SHA256 sha2 = SHA256.Create();
+            {
+                byte[] hashBytes = sha2.ComputeHash(inputBytes);
+
+                // Convert the byte array to hexadecimal string
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < hashBytes.Length; i++)
+                {
+                    sb.Append(hashBytes[i].ToString("X2"));
+                }
+                return sb.ToString();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                switch (comboBox1.SelectedIndex)
+                {
+                    case 0:
+                        {
+                            arrayBytes = GetBytesFromBinaryString(textBox.Text);
+                            break;
+                        }
+                    case 1:
+                        {
+                            arrayBytes= ASCIIEncoding.ASCII.GetBytes(textBox.Text);
+                            break;
+                        }
+                    case 2:
+                        {
+                            arrayBytes = FromHexString(textBox.Text);
+                            break;
+                        }
+                }
+                if (checkBox1.Checked)
+                {
+                    tbMD5.Text = CreateMD5(arrayBytes);
+                }
+                if (checkBox2.Checked)
+                {
+                    tbSHA1.Text = CreateSHA1(arrayBytes);
+                }
+                if (checkBox3.Checked)
+                {
+                    tbSHA256.Text = CreateSHA2(arrayBytes);
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Thông tin đầu vào lỗi hoặc định dạng không đúng");
+            }
+        }
+    }
+}
